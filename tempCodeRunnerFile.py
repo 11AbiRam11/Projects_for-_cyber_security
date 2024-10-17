144f7d0bcc19699eda427a1e3a08cbc4caedbc5d
11AbiRam11 <abhi555rram@gmail.com>
2024-10-17 16:14:21 +0530

    resotred my crappy passwd encrypter

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+for i in range(65,78):
+#     fd.update({chr(i):j})
+#     j-=1
+
+# for i in range(123,127):
+#     fd.update({chr(i):j})
+#     j-=1
+
+# for i in range(78,91):
+#     fd.update({chr(i):k})
+#     k-=1
+
+# for i in range(91,97):
+#      fd.update({chr(i):j})
+#      k+=1
+    
